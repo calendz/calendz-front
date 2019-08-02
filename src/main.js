fca69fad236e6605df9a2922b4bbe9d62ae4dfa5
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-08-02 17:24:54 +0200

    refactor(storage.service): remove storage.service

--- src/main.js
+++ src/main.js
@@ -4,18 +4,12 @@ import router from './routes/router'
 import DashboardPlugin from './plugins/dashboard-plugin'
 import { VueSpinners } from '@saeris/vue-spinners'
 import ApiService from './services/api.service'
-import TokenService from './services/storage.service'
 
 Vue.use(DashboardPlugin)
 Vue.use(VueSpinners)
 
 // Set the base URL of the API
 ApiService.init(process.env.VUE_APP_API_URL)
-
-// If token exists set header
-if (TokenService.getToken()) {
-  ApiService.setHeader()
-}
 
 /* eslint-disable no-new */
 new Vue({
