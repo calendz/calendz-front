8ce65f7a736ead9031cd222af5d59edfa610a727
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-08-15 10:49:52 +0200

    feat(dashboard-admin): add dashboard-admin page

---
+++ src/views/Pages/Dashboard/Admin/DashboardAdmin.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- ======================================= -->
+    <!-- == "Base header" ====================== -->
+    <!-- ======================================= -->
+    <base-header
+      type="primary"
+      class="pb-6">
+      <div class="row align-items-center py-4">
+        <div class="col-lg-6 col-7">
+          <h6 class="h2 text-white d-inline-block mb-0">Dashboard</h6>
+          <nav
+            aria-label="breadcrumb"
+            class="d-none d-md-inline-block ml-md-4">
+            <route-bread-crumb/>
+          </nav>
+        </div>
+      </div>
+    </base-header>
+
+    <!-- ======================================= -->
+    <!-- == Main =============================== -->
+    <!-- ======================================= -->
+    <div class="container-fluid mt--6">
+      <div class="row">
+        <div class="col-lg-4">
+          <h1 class="text-white">Dashboard admin</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import RouteBreadCrumb from '@/components/Breadcrumb/RouteBreadcrumb'
+
+export default {
+  components: {
+    RouteBreadCrumb
+  }
+}
+</script>
