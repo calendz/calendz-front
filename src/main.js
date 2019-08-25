c0b8ca5beab22a5ec758b32ac1a2ec5cf6abedc9
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-08-25 16:59:36 +0200

    feat(analytics): add vue-analytics config for ga

--- src/main.js
+++ src/main.js
@@ -5,9 +5,21 @@ import router from './routes/router'
 import DashboardPlugin from './plugins/dashboard-plugin'
 import ApiService from './services/api.service'
 import { VueSpinners } from '@saeris/vue-spinners'
+import VueAnalytics from 'vue-analytics'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 Vue.use(DashboardPlugin)
 Vue.use(VueSpinners)
+
+Vue.use(VueAnalytics, {
+  id: 'UA-146470801-1',
+  router,
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd
+  }
+})
 
 // Set the base URL of the API
 ApiService.init(process.env.VUE_APP_API_URL)
