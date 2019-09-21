628cab4edb458a8d7665d973a05186350315b9de
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-09-21 18:58:04 +0200

    feat(naviguation): replaced placeholders by redirections to the documentation

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -34,10 +34,6 @@ const UserManagement = () => import(/* webpackChunkName: "page" */ '@/views/Page
 
 const Notifications = () => import(/* webpackChunkName: "page" */ '@/views/Pages/Dashboard/Hidden/Notifications.vue')
 
-const Blog = () => import(/* webpackChunkName: "page" */ '@/views/Pages/Dashboard/Blog.vue')
-const Features = () => import(/* webpackChunkName: "page" */ '@/views/Pages/Dashboard/Features.vue')
-const BugReport = () => import(/* webpackChunkName: "page" */ '@/views/Pages/Dashboard/BugReport.vue')
-
 // ============================================
 // == Routes
 // ============================================
@@ -138,33 +134,6 @@ const dashboardPages = {
       component: Notifications,
       meta: {
         title: 'Notifications - Calendz'
-      }
-    },
-    // =================================
-    // == documentation tab
-    // =================================
-    {
-      path: 'blog',
-      name: 'Blog',
-      component: Blog,
-      meta: {
-        title: 'Dashboard - Calendz'
-      }
-    },
-    {
-      path: 'features',
-      name: 'Features',
-      component: Features,
-      meta: {
-        title: 'Dashboard - Calendz'
-      }
-    },
-    {
-      path: 'bug-report',
-      name: 'Signaler un bug',
-      component: BugReport,
-      meta: {
-        title: 'Dashboard - Calendz'
       }
     }
   ]
