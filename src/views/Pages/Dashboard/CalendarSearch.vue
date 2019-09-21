f71eb89da577b7a5d2d527a4cc1da86bbfa2d4f8
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-09-21 19:32:15 +0200

    feat(coming-soon): add temp "feat. comming soon" page

--- src/views/Pages/Dashboard/CalendarSearch.vue
+++ src/views/Pages/Dashboard/CalendarSearch.vue
@@ -1,5 +1,43 @@
 <template>
   <div>
-    <h1>Emploi du temps - Recherche</h1>
+    <!-- ======================================= -->
+    <!-- == "Base header" ====================== -->
+    <!-- ======================================= -->
+    <base-header
+      type="primary"
+      class="pb-6">
+      <div class="row align-items-center py-4">
+        <div class="col-lg-6 col-7">
+          <h6 class="h2 text-white d-inline-block mb-0">Emploi du temps</h6>
+          <nav
+            aria-label="breadcrumb"
+            class="d-none d-md-inline-block ml-md-4">
+            <route-bread-crumb/>
+          </nav>
+        </div>
+      </div>
+    </base-header>
+
+    <div class="container-fluid mt--6 card-wrapper">
+      <div
+        class="row"
+        style="margin-top: 20%; transform: translateY(-20%); margin-bottom: 12%">
+        <card
+          type="frame"
+          class="col-10 mx-auto text-center">
+          <h1 class="text-default">Cette fonctionnalité n'est pas encore disponible !</h1>
+          <h4 class="font-weight-300">Mais elle arrivera très prochainement, donc restez attentif !</h4>
+        </card>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import RouteBreadCrumb from '@/components/Breadcrumb/RouteBreadcrumb'
+
+export default {
+  components: {
+    RouteBreadCrumb
+  }
+}
+</script>
