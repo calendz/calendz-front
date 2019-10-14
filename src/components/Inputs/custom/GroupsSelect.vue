118dc0881b49f9c1ea7f1c4d83db94a162a4aa31
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-10-14 18:37:37 +0200

    feat(groups-select): added a "legacy" select which doesn't use element-ui

--- src/components/Inputs/custom/GroupsSelect.vue
+++ src/components/Inputs/custom/GroupsSelect.vue
@@ -1,21 +1,54 @@
 <template>
-  <base-input
-    :error="getError('groupe')"
-    :valid="isValid('groupe')"
-    class="w-100"
-    label="Groupe">
-    <el-select
-      v-validate="'required|valid_group'"
-      v-model="inputValue"
-      name="groupe"
-      disabled>
-      <el-option
-        v-for="(option, index) in options"
-        :key="index"
-        :label="option.value"
-        :value="option.value"/>
-    </el-select>
-  </base-input>
+  <div>
+
+    <!-- =================================== -->
+    <!-- == ELEMENT-UI'S SELECT ============ -->
+    <!-- =================================== -->
+    <base-input
+      v-if="!legacy"
+      :error="getError('groupe')"
+      :valid="isValid('groupe')"
+      :label="label"
+      class="w-100">
+      <el-select
+        v-validate="'required|valid_group'"
+        v-model="inputValue"
+        :disabled="disabled"
+        name="groupe">
+        <el-option
+          v-for="(option, index) in options"
+          :key="index"
+          :label="option.value"
+          :value="option.value"/>
+      </el-select>
+    </base-input>
+
+    <!-- =================================== -->
+    <!-- == LEGACY SELECT ================== -->
+    <!-- =================================== -->
+    <base-input
+      v-if="legacy"
+      :error="getError('groupe')"
+      :valid="isValid('groupe')"
+      :label="label"
+      class="mb-3"
+      prepend-icon="ni ni-hat-3">
+      <select
+        v-validate="'required|valid_group'"
+        v-model="inputValue"
+        :disabled="disabled"
+        name="groupe"
+        class="form-control">
+        <option
+          value=""
+          hidden>Séléctionnez votre groupe</option>
+        <option
+          v-for="(option, index) in options"
+          :key="index"
+          :value="option.value">{{ option.value }}</option>
+      </select>
+    </base-input>
+  </div>
 </template>
 
 <script>
@@ -33,6 +66,21 @@ export default {
       type: String,
       default: '',
       doc: 'Value'
+    },
+    disabled: {
+      type: Boolean,
+      default: true,
+      doc: 'Whether the select should be disabled or not'
+    },
+    label: {
+      type: String,
+      default: '',
+      doc: `Ìnput's label`
+    },
+    legacy: {
+      type: Boolean,
+      default: false,
+      doc: `Whether we should use a legacy select or element-ui's select`
     }
   },
   data () {
