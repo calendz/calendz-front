c74c7ff781041a3f83cbdf2f2043923bcebd67fc
Arthur Dufour <dufourarthur.perso@gmail.com>
2019-10-30 11:34:36 +0100

    fix(settings): fixed route-bread-crumb name

--- src/views/Pages/Dashboard/Settings.vue
+++ src/views/Pages/Dashboard/Settings.vue
@@ -8,7 +8,7 @@
       class="pb-6">
       <div class="row align-items-center py-4">
         <div class="col-lg-6 col-7">
-          <h6 class="h2 text-white d-inline-block mb-0">Dashboard</h6>
+          <h6 class="h2 text-white d-inline-block mb-0">Paramètres</h6>
           <nav
             aria-label="breadcrumb"
             class="d-none d-md-inline-block ml-md-4">
