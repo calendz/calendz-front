99845d609d2d1fb79edd9c08861811794f56cd34
Arthur Dufour <dufourarthur.perso@gmail.com>
2020-08-31 14:54:24 +0200

    feat(grade): added menu link and blank page

---
+++ src/views/Pages/Dashboard/Grades.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- ======================================= -->
+    <!-- == "Base header" ====================== -->
+    <!-- ======================================= -->
+    <base-header
+      type="primary"
+      class="pb-6">
+      <div class="row align-items-center py-4">
+        <div class="col-lg-6 col-7">
+          <h6 class="h2 text-white d-inline-block mb-0">Gestion des notes</h6>
+          <nav
+            aria-label="breadcrumb"
+            class="d-none d-md-inline-block ml-md-4">
+            <route-bread-crumb/>
+          </nav>
+        </div>
+      </div>
+    </base-header>
+
+    <!-- ======================================= -->
+    <!-- == Main container ===================== -->
+    <!-- ======================================= -->
+    <!-- <div class="container-fluid mt--6 card-wrapper">
+      <div class="card mb-4">
+        <div class="container-fluid mt-4">
+          <div class="row">
+            <div/>
+          </div>
+        </div>
+      </div>
+    </div> -->
+  </div>
+</template>
